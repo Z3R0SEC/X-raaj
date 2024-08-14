6d814ae5e194ff1945c3383eff0b5f80c391782e
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-08-14 17:41:44 +0200

    Create raaj.js

---
+++ assets/plugins/raaj.js
@@ -0,0 +1,33 @@
+const { command, isPrivate } = require("../../lib");
+const axios = require("axios");
+                                                           
+command(
+  {
+    pattern: "raaj",
+    fromMe: isPrivate,                                         desc: "Chat With Raaj AI",
+    type: "ai",
+  },
+  async (message, match, m) => {
+    const prompt = match.trim();                               const uid = message.sender; // Assuming uid is the sender's ID
+
+    if (!prompt) {
+      return message.reply("*_Whats New ?_*");
+    }
+
+    try {
+      const response = await axios.post("https://mota-dev.x10.bz/ai", {
+        prompt: prompt,
+        uid: uid,
+      });
+
+      if (response.data && response.data.reply) {
+        message.reply(`*${response.data.reply}*`);
+      } else {
+        message.reply("*Sorry I Dont Have Answer To Your Message*");
+      }
+    } catch (error) {
+      console.error("*Error interacting with AI API:*", error.message);
+      message.reply(`*_X-RaaJ-K_*\n\n${error.message}`);
+    }
+  }
+);
